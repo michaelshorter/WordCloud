c24b83a5996ee72a2bc6e3de40b3bf26750eedc9
mikevanis <hello@mikevanis.com>
2022-07-14 16:10:10 +0100

    make word cloud generator run in a loop

--- WordCloudGenerator.py
+++ WordCloudGenerator.py
@@ -3,6 +3,7 @@ import os
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import time
 
 dirname = os.path.dirname(__file__)
 content_path = os.path.join(dirname, 'AzureSpeechCC/content.txt')
@@ -13,29 +14,37 @@ dither_image_what_path = os.path.join(dirname, 'dither-image-what.py')
 def grey_color_func(word, font_size, position,orientation,random_state=None, **kwargs):
     return "black" 
 
-# read text from file and store in a variable
-with open(content_path, 'r') as file:
-    data = file.read()
+def generate_wordcloud_from_file(file_path):
+    # read text from file and store in a variable
+    with open(file_path, 'r') as file:
+        data = file.read()
 
-print(data)
+    # create wordcloud using data
+    wordcloud = WordCloud(
+        background_color="white", height=300, width=400,
+        include_numbers = True, min_word_length=6, # minimum length of word
+        max_words = 15, margin = 4 # margin between words
+    ).generate(data)
 
-# create wordcloud using data
-wordcloud = WordCloud(
-    background_color="white", height=300, width=400,
-    include_numbers = True, min_word_length=6, # minimum length of word
-    max_words = 15, margin = 4 # margin between words
-).generate(data)
+    default_colors = wordcloud.to_array()
 
-default_colors = wordcloud.to_array()
+    plt.imshow(wordcloud.recolor(color_func=grey_color_func), interpolation="bilinear")
 
-plt.imshow(wordcloud.recolor(color_func=grey_color_func),
-interpolation="bilinear")
+    plt.axis("off")
+    plt.savefig(wordcloud_image_path)
+    #plt.savefig('/home/pi/latestWordCloud.png')
+    #plt.show()
 
-plt.axis("off")
-plt.savefig(wordcloud_image_path)
-#plt.savefig('/home/pi/latestWordCloud.png')
-#plt.show()
+    # 
 
-#
-os.system("python3 " + dither_image_what_path + " --colour 'red' --image '" + wordcloud_image_path + "'")
+def main():
+    while True:
+        # Generate wordcloud from content file
+        generate_wordcloud_from_file(content_path)
+        # Write image to e-ink display
+        os.system("python3 " + dither_image_what_path + " --colour 'red' --image '" + wordcloud_image_path + "'")
+        # Sleep
+        time.sleep(100)
 
+if __name__ == "__main__":
+    main()
