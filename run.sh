a2ae783d16f43a09a94492a15ad6e1f1fd4485ef
michaelshorter <mail@mrshorter.co.uk>
2022-07-15 11:45:09 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo python3 WordCloudGenerator.py & cd AzureSpeechCC && dotnet run && sleep 10s && cd .. && sudo python write_to_oled.py
+sudo python3 WordCloudGenerator.py & cd AzureSpeechCC && dotnet run & sleep 10s && sudo python write_to_oled.py
